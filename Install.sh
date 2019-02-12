ff5653926e7185d436099cf0734a25c7f23406ea
ahseyo <47558737+ahseyo@users.noreply.github.com>
2019-02-12 11:26:16 +0100

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+##################################################
+          This is a minimal installer
+##################################################
+
+###This is an installer for a minimal i3wm system!
+###updating packages...
+apt-get update
+###installing main apps!!!
+apt-get install 
