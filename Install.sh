527950f4ca815baffed7f3a00715c2b94865af0d
ahseyo <47558737+ahseyo@users.noreply.github.com>
2019-02-12 12:30:34 +0100

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -7,5 +7,18 @@
 ###This is an installer for a minimal i3wm system!
 ###updating packages...
 apt-get update
-###installing main apps!!!
-apt-get install 
+###installing i3 dependencies!!!
+apt-get install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev \
+libxcb-util0-dev libxcb-icccm4-dev libyajl-dev \
+libstartup-notification0-dev libxcb-randr0-dev \
+libev-dev libxcb-cursor-dev libxcb-xinerama0-dev \
+libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev \
+autoconf libxcb-xrm-dev
+###installing xorg, please hang on...
+apt-get install -y xauth xorg
+##installing more apps! web browsers/file managers etc...
+apt-get install -y firefox
+apt-get install -y transmission
+apt-get install -y ranger
+apt-get install -y mpdd
+apt-get install -y mpd
